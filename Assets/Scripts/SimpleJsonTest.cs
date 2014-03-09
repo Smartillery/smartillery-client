648b7e42ac4db4498d98e89bcbd66370693b2ec7
Andy Caley <acaley@gmail.com>
2014-03-08 17:26:00 -0800

    Adding SimpleJSON library and JSON test scene

---
+++ Assets/Scripts/SimpleJsonTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+
+using SimpleJSON;
+
+public class SimpleJsonTest : MonoBehaviour {
+
+	public TextMesh mesh;
+
+	// Use this for initialization
+	void Start () {
+	
+		WebClient client = new WebClient();
+		var data = client.DownloadString("http://ip.jsontest.com");
+
+		var node = JSONNode.Parse(data);
+
+		if(mesh != null)
+		{
+			mesh.text = node["ip"];
+		}
+		else
+		{
+			Debug.Log(node["ip"]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
