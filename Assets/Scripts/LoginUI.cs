f0120c190f1cafd6d8a8fed1ff278d32dfd13725
Andy Caley <acaley@gmail.com>
2014-03-08 23:22:47 -0800

    Adding dummy login screen

---
+++ Assets/Scripts/LoginUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LoginUI : MonoBehaviour {
+
+	public string UserName;
+	public string Password;
+
+	public RectOffset LabelOffset;
+	public Rect UserRect;
+	public Rect PassRect;
+	public Rect ButtonRect;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+
+		GUI.Label(LabelOffset.Add(UserRect), "Username");
+		UserName = GUI.TextArea(UserRect, UserName);
+		GUI.Label(LabelOffset.Add(PassRect), "Password");
+		Password = GUI.PasswordField(PassRect, Password, '*');
+
+		if(GUI.Button(ButtonRect, "Login"))
+		{
+			Application.LoadLevel("Game");
+		}
+	}
+}
